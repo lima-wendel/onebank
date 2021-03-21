4d389849a7eeb7c61a0a5633cf9686afa2a80ddf
Wendel Lima <64101945+lima-wendel@users.noreply.github.com>
2021-03-21 20:54:03 -0300

    criando a classe conta

---
+++ src/main/kotlin/com/onebank/wendellima/Conta.kt
@@ -0,0 +1,17 @@
+package com.onebank.wendellima
+
+import java.math.BigDecimal
+
+class Conta {
+    //criar um contrutor primário
+    val agencia: String,
+    val nuemro: String,
+    val saldo: BigDecimal
+} {
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
